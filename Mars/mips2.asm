3cd44b7f18d1bace7ba538a4cbfcfe19bd3f6d71
Samuelj-santos <samuel.00000847429@unicap.br>
2024-06-17 20:29:30 -0300

    MIPS

---
+++ Mars/mips2.asm
@@ -0,0 +1,28 @@
+ lui  $t0,0x1001
+ add  $t0,$t0,0x0000
+ 
+ 
+  addi $t1,$zero,1
+   addi $t2,$zero,2
+    addi $t3,$zero,3
+    
+    sw $t1,0($t0)
+    sw $t2,4($t0)
+    sw $t3,8($t0)
+  
+      
+      
+      
+        
+      
+       
+      
+       
+         
+                                       
+     
+        
+  
+   
+   
+     
